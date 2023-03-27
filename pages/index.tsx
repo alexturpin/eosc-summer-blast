f570483357604298d746cb62c199375061136e4d
Alex Turpin <alex.turpin@gmail.com>
2023-03-27 18:47:59 -0500

    More info + DTS sponsor

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image"
 import { useEffect, useState } from "react"
 
-const logoSize = { width: 580, height: 809 }
-const logoRatio = 0.75
+const logoSize = { width: 580, height: 809, ratio: 0.75 }
+const dtsLogoSize = { width: 868, height: 291, ratio: 0.6 }
 
 const registrationDate = new Date("2023-04-02T20:00:00-04:00")
 
@@ -29,13 +29,13 @@ export default function Home() {
         <h2 className="sr-only">IPSC Level III</h2>
       </header>
 
-      <main className="flex flex-wrap flex-col items-center py-8 px-8">
+      <main className="flex flex-wrap flex-col items-center p-8 pb-16">
         <Image
           className="mb-4"
           src="/logo-v1-trimmed.png"
           alt="logo"
-          width={logoSize.width * logoRatio}
-          height={logoSize.height * logoRatio}
+          width={logoSize.width * logoSize.ratio}
+          height={logoSize.height * logoSize.ratio}
           priority
         />
         <h3 className="text-eosc-blue text-center text-4xl md:text-6xl font-bold mb-8">
@@ -52,7 +52,32 @@ export default function Home() {
           </a>{" "}
           {localDateTime}
         </p>
+
+        <p className="text-eosc-blue text-center text-2xl md:text-4xl font-bold mb-4 max-w-screen-md">
+          More info coming soon...
+        </p>
       </main>
+
+      <section className="p-8">
+        <div className="flex flex-wrap flex-col justify-center items-center gap-4">
+          <h4 className="text-eosc-blue text-center text-2xl md:text-4xl font-bold max-w-screen-md">
+            Match sponsor
+          </h4>
+          <a href="https://www.doubletapsports.com/">
+            <Image
+              src="/dts.png"
+              alt="DoubleTap Sports logo"
+              width={dtsLogoSize.width * dtsLogoSize.ratio}
+              height={dtsLogoSize.height * dtsLogoSize.ratio}
+            />
+          </a>
+        </div>
+        {/*
+        Range sponsor
+
+        Stage sponsor
+        */}
+      </section>
     </>
   )
 }
